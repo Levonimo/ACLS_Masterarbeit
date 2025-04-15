6006df7be89ed586849590e7dd0222da50005431
Levonimo <levinwilli@protonmail.com>
2025-04-15 15:09:32 +0200

    [ fix ] adjusted parameters

--- Plot_Chromatogram.py
+++ Plot_Chromatogram.py
@@ -1,28 +1,33 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import os
 
 # Plotting function
 def plot_chromatogram(data, retention_time, font_size, font_family, label, color):
     # Filter data to retention time window
     filtered = data[(data['time'] >= retention_time[0]) & (data['time'] <= retention_time[1])]
-    plt.plot(filtered['time'], filtered['intensity'], label=label, color=color)
+    plt.plot(filtered['time'], filtered['intensity'], label=label, color=color, linewidth=0.5)
     return filtered  # Rückgabe für Y-Limit-Berechnung
 
 # Parameters
 parameters = dict(
-    data_folder = "F:/Dokumente/Masterarbeit/CSV_all_Sample",
+    #data_folder = "F:/Dokumente/Masterarbeit/CSV_all_Sample",
+    data_folder = "//user.zhaw.ch/staff/wilv/Documents/Masterarbeit/CSV_all_Sample",
     batch_numbers = ["A1"],
-    sample_types = ["all"],
+    sample_types = ["SOO", "SOL", "SGO", "SGL", "OOO"],
     coloring_schema = "Sample",
-    retention_time = [9.9, 10.8],
-    image_height = 7,
-    image_width = 8,
+    retention_time = [8.2  , 9.2],
+    tick_interval = 0.1,
+    y_max = "default", #3*10**7,  "default" or a specific value 
+    image_height = 5,
+    image_width = 6, # 12 cm = textwidth
     dpi = 300,
-    font_size = 10,
+    font_size = 12,
     font_family = "Arial",
-    output_folder = "F:/Dokumente/Masterarbeit/Masterarbeit_Latex/Abbildungen/Results/Indirect",
-    output_file_name = "Chrom_Auschnit_A1_Benzen_Silicium2.png",
+    #output_folder = "F:/Dokumente/Masterarbeit/Masterarbeit_Latex/Abbildungen/Results/Indirect",
+    output_folder = "C:/Users/wilv/Documents/Masterarbeit/Masterarbeit_Latex/Abbildungen/Results/Indirect",
+    output_file_name = "Chrom_Ausschnitt_A1_Xylene.png"
 )
 
 color_schema = {
@@ -68,7 +73,7 @@ for file in os.listdir(parameters["data_folder"]):
 # Berechne globales y-Limit basierend auf allen gefilterten Daten
 all_filtered_concat = pd.concat(filtered_all)
 y_min = all_filtered_concat['intensity'].min()
-y_max = all_filtered_concat['intensity'].max()
+y_max = all_filtered_concat['intensity'].max() if parameters["y_max"] == "default" else parameters["y_max"]
 y_range = y_max - y_min
 if y_range == 0:
     y_range = y_max * 0.1
@@ -79,6 +84,7 @@ plt.xlim(parameters["retention_time"])
 plt.xlabel('Time / min', fontsize=parameters["font_size"], fontfamily=parameters["font_family"])
 plt.ylabel('Intensity', fontsize=parameters["font_size"], fontfamily=parameters["font_family"])
 plt.tick_params(labelsize=parameters["font_size"])
+plt.xticks(np.arange(parameters["retention_time"][0], parameters["retention_time"][1], parameters["tick_interval"]))
 plt.legend()
 plt.gca().spines['right'].set_visible(False)
 plt.gca().spines['top'].set_visible(False)
